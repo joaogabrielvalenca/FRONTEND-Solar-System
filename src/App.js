52c7c7a3f5ef4fed7871a3f82c49373ff343fc68
joaogabrielvalenca <joaogabrielvalenca@gmail.com>
2023-01-31 10:35:00 -0300

    commit proj 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import Title from './components/Title';
 import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+// import MissionCard from './components/MissionCard';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <Title />
         <SolarSystem />
         <Missions />
         {/* <Title headline="planetas" /> */}
